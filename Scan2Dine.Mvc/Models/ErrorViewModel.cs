5ab8e935d7d34a63400cf82ca8598c29ebeb9c76
wangliang <zx_wangliang@bosssoft.com.cn>
2025-02-08 19:10:02 +1300

    Add Mvc project.

---
+++ Scan2Dine.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Scan2Dine.Mvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
